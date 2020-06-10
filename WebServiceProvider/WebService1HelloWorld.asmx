188c69277d71dd5e96814e6b14cc819cebdbd1d0
tsahom <43750088+tsahom@users.noreply.github.com>
2020-06-10 16:34:15 +0200

    init

---
+++ WebServiceProvider/WebService1HelloWorld.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="WebService1HelloWorld.asmx.cs" Class="WebServiceProvider.WebService1HelloWorld" %>
---
+++ WebServiceProvider/WebService1HelloWorld.asmx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebServiceProvider
+{
+    /// <summary>
+    /// Description résumée de WebService1HelloWorld
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Pour autoriser l'appel de ce service Web depuis un script à l'aide d'ASP.NET AJAX, supprimez les marques de commentaire de la ligne suivante. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1HelloWorld : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public string Hello(string name)
+        {
+            return "Hello " + name;
+        }
+
+        [WebMethod]
+        public int Add(int p1, int p2)
+        {
+            return p1 + p2;
+        }
+    }
+}
